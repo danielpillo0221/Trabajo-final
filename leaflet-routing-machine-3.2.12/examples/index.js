54e32aa0fab27234558758fd24337176c4b1f6ea
daniel <carmonadaniel0222gmai.com>
2023-11-28 21:07:38 -0500

    agrege la extension del mapa

---
+++ leaflet-routing-machine-3.2.12/examples/index.js
@@ -0,0 +1,25 @@
+var map = L.map('map');
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+var control = L.Routing.control(L.extend(window.lrmConfig, {
+	waypoints: [
+		L.latLng(5.968408833343811, -75.36062938578465),
+		L.latLng(6.030201, -75.431743)
+	],
+	geocoder: L.Control.Geocoder.nominatim(),
+	routeWhileDragging: true,
+	reverseWaypoints: true,
+	showAlternatives: true,
+	altLineOptions: {
+		styles: [
+			{color: 'black', opacity: 0.15, weight: 9},
+			{color: 'white', opacity: 0.8, weight: 6},
+			{color: 'blue', opacity: 0.5, weight: 2}
+		]
+	}
+})).addTo(map);
+
+L.Routing.errorControl(control).addTo(map);
