f213abbdea67134ca22b738ae8c3246cbfbaa0b4
daniel <carmonadaniel0222gmai.com>
2023-12-10 17:04:34 -0500

    le cambie los estilos a los botones

--- scipt.js
+++ scipt.js
@@ -17,22 +17,26 @@ if(clave==clave2){
 function botoncitos(){
       // Crea los botones que deseas mostrar
   var boton1 = document.createElement("button");
-  boton1.innerHTML = "Botón 1";
+  boton1.innerHTML = "Mis reservas";
   boton1.onclick = function() {
 alert("b")  };
 
   var boton2 = document.createElement("button");
-  boton2.innerHTML = "Botón 2";
+  boton2.innerHTML = "Mi billetera";
   boton2.onclick = function() {
 alert("a")  };
 var boton3 = document.createElement("button");
-boton3.innerHTML = "Botón 3";
+boton3.innerHTML = "Mis pedidos";
 boton3.onclick = function() {
 alert("b")  };
 
 var boton4 = document.createElement("button");
-boton4.innerHTML = "Botón 4";
-boton2.onclick = function() {
+boton4.innerHTML = "Mi cuenta";
+boton4.onclick = function() {
+alert("a")  };
+var boton5 = document.createElement("button");
+boton5.innerHTML = "Cerrar Sesion";
+boton4.onclick = function() {
 alert("a")  };
 
   // Agrega los botones al DOM
@@ -45,16 +49,40 @@ alert("a")  };
 
 
   boton1.style.backgroundColor = "black"; // Cambia el color de fondo a rojo
-boton2.style.backgroundColor = "black"; // Cambia el color de fondo a azul
-boton2.style.color = "white"; // Cambia el color del texto a blanco
-boton1.style.color = "white"; // Cambia el color del texto a blanco
+boton2.style.backgroundColor = "black";// Cambia el color de fondo a rojo
 boton3.style.backgroundColor = "black"; // Cambia el color de fondo a rojo
 boton4.style.backgroundColor = "black"; // Cambia el color de fondo a azul
+ // Cambia el color de fondo a azul
+boton2.style.color = "white"; // Cambia el color del texto a blanco
+boton1.style.color = "white"; // Cambia el color del texto a blanco
+// boton5.style.backgroundColor = "black"; // Cambia el color de fondo a azul
 boton3.style.color = "white"; // Cambia el color del texto a blanco
 boton4.style.color = "white"; // Cambia el color del texto a blanco
-boton1.style.border
-boton2.style.border
-boton3.style.border
-boton4.style.border
+// boton5.style.color = "white"; // Cambia el color del texto a blanco
+boton1.style.border = "none";
+boton2.style.border = "none";
+boton3.style.border = "none";
+boton4.style.border = "none";
+// boton5.style.border = "none";
+boton1.style.padding = "15px ";
+boton2.style.padding = "15px ";
+boton3.style.padding = "15px ";
+boton4.style.padding = "15px ";
+// boton5.style.padding = "15px 40px";
+boton1.style.fontSize = "14px";
+boton2.style.fontSize = "14px";
+boton3.style.fontSize = "14px";
+boton4.style.fontSize = "14px";
+// boton5.style.fontSize = "12px";
+boton1.style.cursor ="pointer";
+boton2.style.cursor ="pointer";
+boton3.style.cursor ="pointer";
+boton4.style.cursor ="pointer";
+// boton5.style.cursor ="pointer";
+boton1.style.width="180px"
+boton2.style.width="180px"
+boton3.style.width="180px"
+boton4.style.width="180px"
+boton5.style.width="180px"
 }
 
