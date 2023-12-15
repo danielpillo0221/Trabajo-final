f0f9cbd75c18c53542b21a8323143d8eaec36621
daniel <carmonadaniel0222gmai.com>
2023-12-15 12:41:51 -0500

    cree el iniciar sesoin y corregi el required

--- scipt.js
+++ scipt.js
@@ -1,24 +1,24 @@
-function crear() {
-  const nombre = document.getElementById("nombre-input").value;
-  const apellido = document.getElementById("apellido-input").value;
-  const correo = document.getElementById("correo-input").value;
-  const clave = document.getElementById("clave-input").value;
-  const clave2 = document.getElementById("clave2-input").value;
-  if (
-    nombre == [] ||
-    apellido == [] ||
-    correo == [] ||
-    clave == [] ||
-    clave2 == []
-  ) {
-    alert("tienes que llenar todos los campos");
-  }
-  if (clave == clave2) {
-    alert("continuaras...");
-  } else {
-    alert("verifica tu contraseña");
-  }
-}
+// function crear() {
+//   const nombre = document.getElementById("nombre-input").value;
+//   const apellido = document.getElementById("apellido-input").value;
+//   const correo = document.getElementById("correo-input").value;
+//   const clave = document.getElementById("clave-input").value;
+//   const clave2 = document.getElementById("clave2-input").value;
+//   if (
+//     nombre == [] ||
+//     apellido == [] ||
+//     correo == [] ||
+//     clave == [] ||
+//     clave2 == []
+//   ) {
+//     alert("tienes que llenar todos los campos");
+//   }
+//   if (clave == clave2) {
+//     alert("continuaras...");
+//   } else {
+//     alert("verifica tu contraseña");
+//   }
+// }
 function botoncitos() {
   const botonPrincipal = document.getElementById('boton-principal');
   const botonesAdicionales = document.getElementById('botones-adicionales');
@@ -30,4 +30,23 @@ function botoncitos() {
       botonesAdicionales.style.display = 'none';
     }
   });
+}
+function reserva(){
+  alert("A")
+}
+function billetera(){
+  alert("Ab")
+
+}
+function pedidos(){
+  alert("Ac")
+
+}
+function subs(){
+  alert("Ad")
+
+}
+function cuenta(){
+  alert("Ae")
+
 }
